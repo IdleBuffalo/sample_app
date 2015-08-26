be2b7a17a7d2ab912a22c55fe0b9cea7dc9bccda
Dejan Gajsek <dgajsek@gmail.com>
2015-08-26 13:38:25 +0000

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new    # ustvari se nov user 
+  end
+  
+  def create
+    @user = User.new(user_params)   #ta @user se bo uporablkal v new.html.erb
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!" #se bo pojavilo samo enkrat 
+      # Handle a Successful save
+      redirect_to @user            # originally user_url(@user)
+    else
+      render 'new' # renders new template (new.html.erb in views)
+    end
+  end
+  
+  private
+  
+      def user_params
+        params.require(:user).permit(:name, :email, :password,    #strong parameters technique
+                                     :password_confirmation)
+      end
 end
